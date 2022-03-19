1d5035e2870dc3ca3b74879ab93e58ddf12f91d0
Gilyazov Marat <giliyazovmarat@gmail.com>
2022-03-19 23:32:09 +0300

    upd

--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 from text_mood import profanity_check, text_mood
 
 if __name__ == "__main__":
-    text = "Я купил новую машину. Она мне очень нравится"
+    text = "Я купил новую машину. Она мне очень не нравится"
     print(profanity_check(text))
     print(text_mood(text))
