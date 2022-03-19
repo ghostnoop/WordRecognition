96d234c889573fa7ede6f2703d4133c5e21bf0ea
Gilyazov Marat <giliyazovmarat@gmail.com>
2022-03-20 00:16:34 +0300

    upd

--- test_main.py
+++ test_main.py
@@ -3,6 +3,6 @@ from text_mood import profanity_check, text_mood
 
 if __name__ == "__main__":
     text = "Я купил новую машину. Она плохая 😀 мандей"
-    print(profanity_check(text))
+    print(profanity_check(text),'profanity_check')
     print(text_mood(text))
     print(extract_emojis(text))
