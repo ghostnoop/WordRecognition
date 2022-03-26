3898f9f2fe6a42638f89ff6d21aaafc097d72aaa
Gilyazov Marat <giliyazovmarat@gmail.com>
2022-03-27 01:08:14 +0300

    upd

--- test3.py
+++ test3.py
@@ -58,10 +58,10 @@ async def async_worker(connections: dict, main_connection: Connection, processes
             comment: Comment = main_connection.recv()
             s = f'[ {counter} of {size} ] = '
 
-            # print(s, 'main', comment.id, comment.emotion_text_type_id,
-            #       comment.is_contain_profanity, comment.emoji,
-            #       comment.text.replace('\n', ''))
-            # await comment.save()
+            print(s, 'main', comment.id, comment.emotion_text_type_id,
+                  comment.is_contain_profanity, comment.emoji,
+                  comment.text.replace('\n', ''))
+            await comment.save()
             counter += 1
 
 
