e6681488a86a945fedc2c536fb52cd558bb3090d
Gilyazov Marat <giliyazovmarat@gmail.com>
2022-03-26 19:43:59 +0300

    upd

--- db.py
+++ db.py
@@ -10,7 +10,7 @@ class Config:
     DB_USER = os.environ.get('DB_USER')
     DB_PASSWORD = os.environ.get('DB_PASSWORD')
     DB_HOST = os.environ.get('DB_HOST')
-    DB_PORT = os.environ.get('DB_PORT')
+    DB_PORT = int(os.environ.get('DB_PORT'))
     DB_NAME = os.environ.get('DB_NAME')
     DB_DRIVER = os.environ.get('DB_DRIVER')
 
