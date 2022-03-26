4645deab3309946bcade08f40303e0487e10327a
Gilyazov Marat <giliyazovmarat@gmail.com>
2022-03-27 00:48:06 +0300

    upd

--- worker.py
+++ worker.py
@@ -1,3 +1,5 @@
+import sys
+
 from language_detection import detect_language
 from modules.emoji_service import extract_emojis
 
@@ -9,10 +11,13 @@ emotion_dct = {
 
 
 def main_work(text):
-    profanity, mood = detect_language(text)
-    emojis = extract_emojis(text)
     try:
-        mood = emotion_dct[mood.lower()]
-    except:
-        mood = 3
-    return profanity, mood, emojis
+        profanity, mood = detect_language(text)
+        emojis = extract_emojis(text)
+        try:
+            mood = emotion_dct[mood.lower()]
+        except:
+            mood = 3
+        return profanity, mood, emojis
+    except Exception as e:
+        print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
